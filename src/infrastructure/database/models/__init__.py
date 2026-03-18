953003806dfdba9fc12ae966cbce70cc1145c6d1
Chthonic-Galaxy <162060169+Chthonic-Galaxy@users.noreply.github.com>
2026-03-18 16:36:32 -0300

    refactor: removed old 'database.py' and added import field of DB Model

--- src/infrastructure/database/models/__init__.py
+++ src/infrastructure/database/models/__init__.py
@@ -1,7 +1,9 @@
 __all__ = (
     "Base",
     "Site",
+    "Metric",
 )
 
 from src.infrastructure.database.models.base import Base
+from src.infrastructure.database.models.metric import Metric
 from src.infrastructure.database.models.site import Site
